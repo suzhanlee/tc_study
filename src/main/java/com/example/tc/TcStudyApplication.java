004c75a02797ff7e18cd824cc9bd1e7e0bb770a8
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-07-16 15:33:43 +0900

    Initial commit

---
+++ src/main/java/com/example/tc/TcStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tc/TcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
