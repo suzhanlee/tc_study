9095071a61f3054d0945cf1e457a1caf82feb10c
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-07-19 20:27:09 +0900

    project setting init

---
+++ src/main/java/com/example/tc/spring/config/DataSourceConfig.java
@@ -0,0 +1,39 @@
+package com.example.tc.spring.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.hikari.jdbc-url}")
+    private String jdbcUrl;
+
+    @Value("${spring.datasource.hikari.username}")
+    private String username;
+
+    @Value("${spring.datasource.hikari.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(getHikariConfig());
+    }
+
+    private HikariConfig getHikariConfig() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(jdbcUrl);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+        hikariConfig.setMaximumPoolSize(10);
+        return hikariConfig;
+    }
+
+}
