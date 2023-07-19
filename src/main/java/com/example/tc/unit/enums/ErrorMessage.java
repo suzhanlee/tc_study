71422f27b2f73cef19393a8e1ec971951f3b569e
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-07-19 22:39:00 +0900

    feat : Order Unit Test

--- src/main/java/com/example/tc/unit/enums/ErrorMessage.java
+++ src/main/java/com/example/tc/unit/enums/ErrorMessage.java
@@ -7,7 +7,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum ErrorMessage {
 
-    INVAILD_ITEM_SHOULD_BE_POSITIVE_NUMBER("아이템은 1개 이상 주문 가능 합니다.");
+    INVAILD_ITEM_SHOULD_BE_POSITIVE_NUMBER("아이템은 1개 이상 주문 가능 합니다."),
+
+    INVAILD_ORDER_TIME("현재는 주문 시간이 아닙니다.");
+
 
     private final String description;
 
